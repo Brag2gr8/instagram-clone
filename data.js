6eaae2dd27abdcaec10880c5d3790c3879f5c431
Brag2gr8 <58727101+Brag2gr8@users.noreply.github.com>
2022-10-25 11:09:31 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        image: "images/post-vangogh.jpg",
+        caption: "just took a few mushrooms lol",
+        comment: 2983,
+        likes: 21492,
+        lastSeen: 20,
+        isLiked: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee'
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        image: "images/post-courbet.jpg",
+        caption: "i'm feelin a bit stressed tbh",
+        comment: 1942,
+        likes: 12502,
+        lastSeen: 10,
+        isLiked: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2'
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        image: "images/post-ducreux.jpg",
+        caption: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        comment: 2001,
+        likes: 15137,
+        lastSeen: 15,
+        isLiked: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    }
+]
